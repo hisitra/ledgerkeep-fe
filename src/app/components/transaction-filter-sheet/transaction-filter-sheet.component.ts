7e1ff433a479908e60553fc23c76f2c31a2a296c
shivanshk <shivansh.kuchchal@dltlabs.io>
2020-11-04 15:02:05 +0530

    Added validations in tranasaction filter sheet.

--- src/app/components/transaction-filter-sheet/transaction-filter-sheet.component.ts
+++ src/app/components/transaction-filter-sheet/transaction-filter-sheet.component.ts
@@ -1,4 +1,97 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+import { validation } from '../../../assets/configs.json';
+
+const startDateValidator = (formGroup: FormGroup) => {
+  const startDate = formGroup.controls.startDate;
+
+  // The value from document.getElementByID will not be the latest value
+  // without this setTimeout
+  setTimeout(() => {
+    const node = document.getElementById('startDateInput') as HTMLInputElement;
+
+    if (!node || node.value === '') {
+      startDate.setErrors(null);
+      return;
+    }
+
+    if (!startDate.value || !(startDate.value instanceof Date)) {
+      startDate.setErrors({ invalid: true });
+    } else {
+      startDate.setErrors(null);
+    }
+  }, 0);
+};
+
+const endDateValidator = (formGroup: FormGroup) => {
+  const startDate = formGroup.controls.startDate;
+  const endDate = formGroup.controls.endDate;
+
+  // The value from document.getElementByID will not be the latest value
+  // without this setTimeout
+  setTimeout(() => {
+    const node = document.getElementById('endDateInput') as HTMLInputElement;
+
+    if (!node || node.value === '') {
+      endDate.setErrors(null);
+      return;
+    }
+
+    if (!endDate.value || !(endDate.value instanceof Date)) {
+      endDate.setErrors({ invalid: true });
+    } else if (
+      startDate.value instanceof Date &&
+      endDate.value.getTime() < startDate.value.getTime()
+    ) {
+      endDate.setErrors({ tooLow: true });
+    } else {
+      endDate.setErrors(null);
+    }
+  }, 0);
+};
+
+const startAmountValidator = (formGroup: FormGroup) => {
+  const startAmount = formGroup.controls.startAmount;
+
+  const node = document.getElementById('startAmountInput') as HTMLInputElement;
+
+  if (!node || node.value === '') {
+    startAmount.setErrors(null);
+    return;
+  }
+
+  if (typeof startAmount.value !== 'number' || isNaN(startAmount.value)) {
+    startAmount.setErrors({ invalid: true });
+  } else {
+    startAmount.setErrors(null);
+  }
+};
+
+const endAmountValidator = (formGroup: FormGroup) => {
+  const endAmount = formGroup.controls.endAmount;
+  const startAmount = formGroup.controls.startAmount;
+
+  const node = document.getElementById('endAmountInput') as HTMLInputElement;
+
+  if (!node || node.value === '') {
+    endAmount.setErrors(null);
+    return;
+  }
+
+  if (typeof endAmount.value !== 'number' || isNaN(endAmount.value)) {
+    endAmount.setErrors({ invalid: true });
+  } else if (
+    typeof startAmount.value === 'number' &&
+    !isNaN(startAmount.value) &&
+    endAmount.value < startAmount.value
+  ) {
+    endAmount.setErrors({ tooLow: true });
+  } else {
+    endAmount.setErrors(null);
+  }
+};
 
 @Component({
   selector: 'app-transaction-filter-sheet',
@@ -6,7 +99,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transaction-filter-sheet.component.css'],
 })
 export class TransactionFilterSheetComponent implements OnInit {
-  constructor() {}
+  public filterForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: ActivatedRoute) {
+    this.filterForm = this.formBuilder.group(
+      {
+        startDate: [''],
+        endDate: [''],
+        startAmount: [''],
+        endAmount: [''],
+        category: ['', [Validators.pattern(validation.categoryRegex)]],
+      },
+      {
+        validators: [
+          startDateValidator,
+          endDateValidator,
+          startAmountValidator,
+          endAmountValidator,
+        ],
+      },
+    );
+  }
 
   ngOnInit() {}
+
+  processQuery(): void {}
+
+  clearQuery(): void {}
 }
--- src/app/pages/my-transactions/my-transactions.component.ts
+++ src/app/pages/my-transactions/my-transactions.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
 import { TransactionFilterSheetComponent } from 'src/app/components/transaction-filter-sheet/transaction-filter-sheet.component';
 
 @Component({
@@ -17,7 +18,7 @@ export class MyTransactionsComponent implements OnInit {
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  constructor(private filterSheet: MatBottomSheet) {}
+  constructor(private filterSheet: MatBottomSheet, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
@@ -36,5 +37,11 @@ export class MyTransactionsComponent implements OnInit {
     }
 
     this.filterSheet.open(TransactionFilterSheetComponent);
+  }
+
+  async queryParams(): Promise<{ [key: string]: string }> {
+    return new Promise((resolve, reject) => {
+      this.route.queryParams.subscribe(resolve);
+    });
   }
 }
