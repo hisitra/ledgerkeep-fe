32a334f5304fd701e5ed556797ff47de4c8dd3e0
shivanshkc <shivanshbox@gmail.com>
2020-12-10 00:26:41 +0530

    Signup form UI elements added. Navabar is now sticky and Raised cards now don't appear above navbar.

--- src/app/directives/raise-on-load.directive.ts
+++ src/app/directives/raise-on-load.directive.ts
@@ -8,6 +8,10 @@ export class RaiseOnLoadDirective implements OnInit {
 
   ngOnInit(): void {
     const htmlElement = this.element.nativeElement as HTMLElement;
+
+    // Z-Index -1 keeps the element below toolbar.
+    htmlElement.style.zIndex = '-1';
+
     setTimeout(() => htmlElement.classList.add('mat-elevation-z4'), 10);
   }
 }
