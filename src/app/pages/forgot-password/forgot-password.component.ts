c4a77a3f94fa2a880c05947ebc9cfaff382099cb
shivanshkc <shivanshbox@gmail.com>
2020-12-09 13:04:49 +0530

    Forgot password form validations added.

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConfigService } from 'src/app/services/config.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent implements OnInit {
-  constructor() {}
+  public fpForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private conf: ConfigService) {
+    const configs = this.conf.get();
+
+    this.fpForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(configs.validation.emailRegex)]],
+    });
+  }
 
   ngOnInit(): void {}
 }
