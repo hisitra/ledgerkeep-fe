5e750faf7dd7b6b3b8ac241654a12aa4688872ec
shivanshkc <shivanshbox@gmail.com>
2020-12-09 14:00:34 +0530

    Forgot password complete.

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ConfigService } from 'src/app/services/config.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { AuthkeepService } from 'src/app/services/authkeep.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,8 +11,14 @@ import { ConfigService } from 'src/app/services/config.service';
 })
 export class ForgotPasswordComponent implements OnInit {
   public fpForm: FormGroup;
+  public isLoading = false;
 
-  constructor(private formBuilder: FormBuilder, private conf: ConfigService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private conf: ConfigService,
+    private alert: SnackbarService,
+    private authkeep: AuthkeepService,
+  ) {
     const configs = this.conf.get();
 
     this.fpForm = this.formBuilder.group({
@@ -19,4 +27,26 @@ export class ForgotPasswordComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  async onBtnClick(): Promise<void> {
+    if (this.fpForm.invalid) {
+      return;
+    }
+
+    this.setLoading(true);
+
+    try {
+      await this.authkeep.postUserForgotPasswordLink(this.fpForm.value.email);
+      this.alert.success('Instructions sent.');
+    } catch (err) {
+      this.alert.error(err.message);
+    }
+
+    this.setLoading(false);
+  }
+
+  private setLoading(state: boolean): void {
+    state ? this.fpForm.disable() : this.fpForm.enable();
+    this.isLoading = state;
+  }
 }
--- src/app/services/authkeep.service.ts
+++ src/app/services/authkeep.service.ts
@@ -2,11 +2,6 @@ import { Injectable } from '@angular/core';
 import Authkeep from 'authkeep-client';
 import { ConfigService } from './config.service';
 
-const code2Message = {
-  USER_NOT_FOUND: 'Invalid Credentials.',
-  UNAUTHORIZED: 'Invalid Credentials.',
-};
-
 @Injectable({
   providedIn: 'root',
 })
@@ -22,15 +17,29 @@ export class AuthkeepService {
     try {
       return await this.client.getToken(email, password);
     } catch (err) {
-      throw this.handleError(err);
+      throw this.handleError(err, {
+        USER_NOT_FOUND: 'Invalid Credentials.',
+        UNAUTHORIZED: 'Invalid Credentials.',
+      });
     }
   }
 
-  private handleError(err: Error): Error {
+  public async postUserForgotPasswordLink(email: string): Promise<any> {
+    try {
+      return await this.client.postUserForgotPasswordLink(email);
+    } catch (err) {
+      throw this.handleError(err, {
+        USER_NOT_FOUND: 'No user found with this mail.',
+      });
+    }
+  }
+
+  private handleError(err: Error, code2Message: { [key: string]: string }): Error {
     const message = code2Message[err.message];
     if (message) {
       return new Error(message);
     }
+    console.warn('Unexpected error from backend:', err.message);
     return new Error('Please try again later.');
   }
 }
