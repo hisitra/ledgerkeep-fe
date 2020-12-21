d95fd30df186e139b3507edfc6762d4af00b0e62
shivanshkc <shivanshbox@gmail.com>
2020-12-21 11:50:11 +0530

    Added chart module loading-awaiter method.

--- src/app/components/chart-card/chart-card.component.ts
+++ src/app/components/chart-card/chart-card.component.ts
@@ -12,15 +12,50 @@ export class ChartCardComponent implements OnInit {
   @Input() public title = '';
   @Input() public subtitle = '';
 
+  private isLoaded = false;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    google.charts.load('current', { packages: ['corechart'] });
+    google.charts.setOnLoadCallback(() => {
+      this.isLoaded = true;
+    });
+  }
 
-  public async addPieChart(): Promise<void> {}
+  public async addPieChart(): Promise<void> {
+    await this.waitForModuleLoad();
+  }
 
-  public async addAreaChart(): Promise<void> {}
+  public async addAreaChart(): Promise<void> {
+    await this.waitForModuleLoad();
+  }
 
-  public async addLineChart(): Promise<void> {}
+  public async addLineChart(): Promise<void> {
+    await this.waitForModuleLoad();
+  }
 
-  public async addBarChart(): Promise<void> {}
+  public async addBarChart(): Promise<void> {
+    await this.waitForModuleLoad();
+  }
+
+  private async waitForModuleLoad(): Promise<void> {
+    const checkIntervalMS = 1000;
+    let retryLeft = 100;
+
+    return new Promise((resolve, reject) => {
+      const interval = setInterval(() => {
+        if (this.isLoaded) {
+          clearInterval(interval);
+          return resolve();
+        }
+
+        retryLeft = retryLeft - 1;
+        if (retryLeft <= 0) {
+          clearInterval(interval);
+          return reject(new Error('Failed to load chart module.'));
+        }
+      }, checkIntervalMS);
+    });
+  }
 }
