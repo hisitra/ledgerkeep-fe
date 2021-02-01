baf3be7fea1dc8dafdcec2385a506ec8f7f972f5
shivanshkc <shivanshbox@gmail.com>
2021-02-01 13:10:22 +0530

    Project name corrected.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('lk-fe app is running!');
+    expect(page.getTitleText()).toEqual('ledgerkeep-fe app is running!');
   });
 
   afterEach(async () => {
