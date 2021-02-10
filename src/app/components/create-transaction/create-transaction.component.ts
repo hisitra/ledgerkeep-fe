430dc265394773902490120c04daf07b469c2f5a
shivanshkc <shivanshbox@gmail.com>
2021-02-10 13:24:49 +0530

    Categories now loading in add transaction.

--- src/app/components/create-transaction/create-transaction.component.ts
+++ src/app/components/create-transaction/create-transaction.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { LedgerlensService } from '../../services/ledgerlens.service';
+import { AuthService } from '../../services/auth.service';
+import { SnackService } from '../../services/snack.service';
 
 @Component({
   selector: 'app-create-transaction',
@@ -13,7 +16,12 @@ export class CreateTransactionComponent implements OnInit {
   public isLoading = false;
   public isCategoryLoading = false;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private ledgerlens: LedgerlensService,
+    private authService: AuthService,
+    private snack: SnackService,
+  ) {
     this.txForm = this.formBuilder.group({
       amount: [''],
       amountType: ['debit'],
@@ -23,7 +31,23 @@ export class CreateTransactionComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  async ngOnInit(): Promise<void> {
+    await Promise.all([this.loadCategories()]);
+    this.txForm.get('category')?.setValue('');
+  }
 
   onCreateClick(): void {}
+
+  private async loadCategories(): Promise<void> {
+    this.isCategoryLoading = true;
+    const token = await this.authService.getToken();
+    try {
+      const categories = (await this.ledgerlens.getCategories(token)) as any[];
+      this.catNames = categories.map((cat) => cat.name);
+    } catch (err) {
+      this.snack.error('Failed to load categories.');
+    }
+    this.isCategoryLoading = false;
+    console.log(this.catNames);
+  }
 }
