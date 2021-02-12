cc3723057e18dcbd8448d7674d15b51b29cbee6f
shivanshkc <shivanshbox@gmail.com>
2021-02-12 13:26:28 +0530

    Removed the balance variance line component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,7 +37,6 @@ import { DebitPieComponent } from './pages/debit-pie/debit-pie.component';
 import { CreditPieComponent } from './pages/credit-pie/credit-pie.component';
 import { BalanceLineComponent } from './pages/balance-line/balance-line.component';
 import { TransactionCountLineComponent } from './pages/transaction-count-line/transaction-count-line.component';
-import { BalanceVarianceLineComponent } from './pages/balance-variance-line/balance-variance-line.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { SidebarContentComponent } from './components/sidebar-content/sidebar-content.component';
 import { AuthSuccessComponent } from './pages/auth-success/auth-success.component';
@@ -67,7 +66,6 @@ import { ChartCardComponent } from './components/chart-card/chart-card.component
     CreditPieComponent,
     BalanceLineComponent,
     TransactionCountLineComponent,
-    BalanceVarianceLineComponent,
     ToolbarComponent,
     SidebarContentComponent,
     AuthSuccessComponent,
