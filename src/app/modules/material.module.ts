2136190f7694fe8b5d30a8243e33171c18d78064
shivanshk <shivansh.kuchchal@dltlabs.io>
2021-06-17 16:47:21 +0530

    Toolbar actions added.

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [MatToolbarModule],
-  exports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  exports: [MatToolbarModule, MatButtonModule, MatIconModule],
 })
 export class MaterialModule {}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly tokenKey = 'LK_TOKEN';
+
+  isLoggedIn(): boolean {
+    return localStorage.getItem(this.tokenKey) !== '';
+  }
+}
